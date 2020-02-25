e1251199ac164d92a06d36909ae5f03bfc9e72af
mrql <linus.tlund@gmail.com>
2020-02-25 11:54:45 +0100

    Add project files.

---
+++ CDService/Models/CD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDService.Models
+{
+    public class CD
+    {
+        /* - Primärnyckel - */
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Display(Name = "Release date")] // Ändrar presentationsnamnet av kolumnen.
+        [Required]
+        [DataType(DataType.Date)] // Specificerar datumformatet.
+        public DateTime ReleaseDate { get; set; }
+    }
+}
